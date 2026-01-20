e4735e8da10ee7bde35fad93f87869ecc9010d40
BARSAJIT PATRA <barsajit433@gmail.com>
2026-01-20 21:40:30 +0530

    Implement addition of two integers in Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // TODO: Read two integers
-      
+        int a = 3;
+        int b = 5;
+
+        int sum = a + b;
 
         // TODO: Print their sum  using println
+        System.out.println("Sum of 3 and 5 is: " + sum);
       
         scanner.close();
     }
